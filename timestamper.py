0a73f9303bddb685c2b9f190b54176e60e55bdd4
Rob Eady <robertj.eady@gmail.com>
2022-06-11 10:16:44 +0100

    add support for global hotkeys

--- timestamper.py
+++ timestamper.py
@@ -1,5 +1,6 @@
 import time
 import os
+from global_hotkeys import register_hotkey, start_checking_hotkeys
 
 def fmt_timestamp(timestamp, remark):
     # convert to hours, minutes and seconds
@@ -9,42 +10,54 @@ def fmt_timestamp(timestamp, remark):
     seconds = int(timestamp % 60)
     return f"{hours_text}{minutes:02d}:{seconds:02d} {remark or ''}"
 
-def main():
-    timestamps = []
 
-    filename = input("Enter a filename: ")
-    if not filename.endswith(".txt"):
-        filename += ".txt"
-    desc = input("Enter a description: ")
+class Timestamper:
 
-    # warn if file already exists
-    if os.path.exists(filename):
-        print("Warning, file already exists")
+    def __init__(self):
+        self.filename = input("Enter a filename: ")
+        if not self.filename.endswith(".txt"):
+            self.filename += ".txt"
+        if os.path.exists(self.filename):
+            print("Warning, file already exists")
 
-    # start timing
-    start = time.time()
+        self.desc = input("Enter a description: ")
 
-    while True:
-        # get key pressed by user
-        line = input("Press 'q' to quit and save, r to reset, or a comment to stamp: ")
-        if line == "q":
-            # write timestamps to file then exit
-            with open(filename, "w") as f:
-                if desc:
-                    f.write(desc + "\n")
-                for (timestamp, remark) in timestamps:
-                    f.write(fmt_timestamp(timestamp, remark) + "\n")
-            return
-        elif line == "r":
+        self.reset_timing()
+
+        register_hotkey("t", ["control", "alt"], self.record_timestamp)
+        register_hotkey("r", ["control", "alt"], self.reset_timing)
+        start_checking_hotkeys()
+
+    def reset_timing(self):
+        if hasattr(self, "start"):
             print("Resetting")
-            start = time.time()
-            timestamps = []
-        else:
-            now = time.time()
-            stamp = now - start
-            timestamps.append((stamp, line))
-            print(f"Stamp {len(timestamps)} - {fmt_timestamp(stamp, line)}")
+        self.start = time.time()
+        self.timestamps = []
+
+    def record_timestamp(self, remark = None):
+        now = time.time()
+        stamp = now - self.start
+        self.timestamps.append((stamp, remark))
+        print(f"Stamp {len(self.timestamps)} - {fmt_timestamp(stamp, remark)}")
+
+    def input_loop(self):
+        while True:
+            # get key pressed by user
+            line = input("Press 'q' to quit and save, r to reset, or a comment to stamp:\n")
+            if line == "q":
+                # write timestamps to file then exit
+                with open(self.filename, "w") as f:
+                    if self.desc:
+                        f.write(self.desc + "\n")
+                    for (timestamp, remark) in self.timestamps:
+                        f.write(fmt_timestamp(timestamp, remark) + "\n")
+                print("Saved to", self.filename)
+                return
+            elif line == "r":
+                self.reset_timing()
+            else:
+                self.record_timestamp(line)
 
 
 if __name__ == "__main__":
-    main()
+    Timestamper().input_loop()
